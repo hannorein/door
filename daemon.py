3fbb9b0861bb3b5e592d006e725ed85ebbb45f1f
Hanno Rein <hanno@hanno-rein.de>
2019-04-13 17:26:44 +0000

    extreme

--- daemon.py
+++ daemon.py
@@ -2,20 +2,18 @@ import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # door input
-GPIO.setup(17, GPIO.IN) # movement input
+GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # movement input
 GPIO.setup(27, GPIO.IN) # sound input
 GPIO.setup(22, GPIO.OUT) # RED out
 GPIO.setup(23, GPIO.OUT) # moon out
 GPIO.setup(24, GPIO.IN) # rfid in
+GPIO.setup(7, GPIO.IN, pull_up_down=GPIO.PUD_UP) # button
 GPIO.setup(12, GPIO.OUT) # buzzer
-GPIO.setup(25, GPIO.IN, pull_up_down=GPIO.PUD_UP) # button
+GPIO.setup(18, GPIO.OUT) # extreme buzzer 
 buz = GPIO.PWM(12, 1000)
 GPIO.output(22,1) 
 GPIO.output(23,1) 
 
-last_movement = 0
-last_movement_for_moon = 0
-last_sound = 0
 ring_alarm = -1
 
 def increment_counter(filename):
@@ -29,65 +27,65 @@ def increment_counter(filename):
 
 
 def detect_sound(channel):
-    global last_sound
-    last_sound = time.time()
-    with open("sound_lastminute.txt", "w") as f:
-        f.write("1")
     increment_counter("sound_counter.txt")
 GPIO.add_event_detect(27, GPIO.BOTH, callback=detect_sound)
 
 def detect_movement(channel):
-    global last_movement
-    global last_movement_for_moon
-    last_movement = time.time()
-    last_movement_for_moon = time.time()
-    with open("movement_lastminute.txt", "w") as f:
-        f.write("1")
     increment_counter("movement_counter.txt")
 GPIO.add_event_detect(17, GPIO.BOTH, callback=detect_movement)
+
+def detect_button(channel):
+    global ring_alarm 
+    ring_alarm = -1
+GPIO.add_event_detect(7, GPIO.BOTH, callback=detect_button)
 
 def detect_door(channel):
     global ring_alarm 
     #print("dppr cjhamged", GPIO.input(4))
     if GPIO.input(4)==1:
         # door closes
-        ring_alarm = 0
-        with open("door_lastminute.txt", "w") as f:
-            f.write("0")
+        ring_alarm = -1
+        return
+    if ring_alarm > -1:
+        # alarm currently underway
+        return 
     if GPIO.input(4)==0: # door open
-        if GPIO.input(24)==0 and GPIO.input(25)==1: # not unlocked
+        increment_counter("door_counter.txt")
+        if GPIO.input(24)==0 and GPIO.input(7)==1: # not unlocked
             time.sleep(0.2)
             if GPIO.input(4)==0: # door still open (not a fluke)
                 if ring_alarm==-1: # no repeat
-                    ring_alarm = 55
+                    ring_alarm = 90
                     increment_counter("alarm_counter.txt")
-        with open("door_lastminute.txt", "w") as f:
-            f.write("1")
 GPIO.add_event_detect(4, GPIO.BOTH, callback=detect_door)
 
 
 while True:
-    if last_movement_for_moon!=0: 
-        GPIO.output(23,0) # turn on moon 
-        if time.time() - last_movement_for_moon > 2:
-            GPIO.output(23,1) # turn off moon 
-            last_movement_for_moon = 0
-    if last_movement!=0 and time.time() - last_movement > 60:
-        with open("movement_lastminute.txt", "w") as f:
-            f.write("0")
-        last_movement = 0
-    if last_sound!=0 and time.time() - last_sound > 60:
-        with open("sound_lastminute.txt", "w") as f:
-            f.write("0")
-        last_sound = 0
-
+    #if GPIO.input(17): 
+    #    GPIO.output(23,0) # turn on moon 
+    #else:
+    #    GPIO.output(23,1) # turn off moon 
     if ring_alarm>-1:
         ring_alarm -=1
     if ring_alarm>=0:
-        if ring_alarm%2==1:
+        if ring_alarm>=50 and ring_alarm%6!=0:
+            #sparse alarm initially
+            pass
+        else:
             buz.start(50)
             GPIO.output(22,0) 
-        else:
+            if ring_alarm<25:
+                GPIO.output(18,1)
+            time.sleep(0.25)
+            GPIO.output(18,0) # always turn off extreme buzzer as precaution
             buz.stop()
             GPIO.output(22,1) 
+
+        #if ring_alarm%2==1:
+        #    buz.start(50)
+        #    GPIO.output(22,0) 
+        #else:
+        #    buz.stop()
+        #    GPIO.output(22,1) 
+    GPIO.output(18,0) # always turn off extreme buzzer as precaution
     time.sleep(0.1)
--- log.bash
+++ log.bash
@@ -1,33 +1,42 @@
 #!/bin/bash
 export TZ="EST"
 rrdtool update temperature.rrd N:`python temp.py`
-rrdtool update door.rrd N:`cat door_lastminute.txt`
-rrdtool update movement.rrd N:`cat movement_lastminute.txt`
-rrdtool update sound.rrd N:`cat sound_lastminute.txt`
+rrdtool update door_counter.rrd N:`cat door_counter.txt`
 rrdtool update sound_counter.rrd N:`cat sound_counter.txt`
 rrdtool update movement_counter.rrd N:`cat movement_counter.txt`
+rrdtool update alarm_counter.rrd N:`cat alarm_counter.txt`
 
 
-rrdtool graph temperature.png -v "temp" --start -86400 --end now DEF:myspeed=temperature.rrd:temp:AVERAGE LINE2:myspeed#FF0000
-rrdtool graph door.png -v "door" --start -86400 --end now DEF:myspeed2=door.rrd:door:MAX LINE2:myspeed2#00FF00
-rrdtool graph movement.png -v "movement" --start -86400 --end now DEF:myspeed2=movement.rrd:movement:MAX LINE2:myspeed2#00FF00
-rrdtool graph sound.png -v "sound" --start -86400 --end now DEF:myspeed2=sound.rrd:door:MAX LINE2:myspeed2#00FF00
 
-rrdtool graph temperature_hour.png -v "temp" --start -3600 --end now DEF:myspeed=temperature.rrd:temp:AVERAGE LINE2:myspeed#FF0000
-rrdtool graph door_hour.png -v "door" --start -3600 --end now DEF:myspeed2=door.rrd:door:MAX LINE2:myspeed2#00FF00
-rrdtool graph movement_hour.png -v "movement" --start -3600 --end now DEF:myspeed2=movement.rrd:movement:MAX LINE2:myspeed2#00FF00
-rrdtool graph sound_hour.png -v "sound" --start -3600 --end now DEF:myspeed2=sound.rrd:door:MAX LINE2:myspeed2#00FF00
+rrdtool graph temperature_hour.png -v "temp" --start -3600 --end now DEF:temperature=temperature.rrd:temperature:AVERAGE LINE2:temperature#FF0000
+rrdtool graph temperature.png -v "temp" --start -86400 --end now DEF:temperature=temperature.rrd:temperature:AVERAGE LINE2:temperature#FF0000
+rrdtool graph temperature_week.png -v "temp" --start -604800 --end now DEF:temperature=temperature.rrd:temperature:AVERAGE LINE2:temperature#FF0000
+rrdtool graph door_hour.png -v "door" --start -3600 --end now DEF:door=door_counter.rrd:door:MAX LINE2:door#00FF00
+rrdtool graph door.png -v "door" --start -86400 --end now DEF:door=door_counter.rrd:door:MAX LINE2:door#00FF00
+rrdtool graph door_week.png -v "door" --start -604800 --end now DEF:door=door_counter.rrd:door:MAX LINE2:door#00FF00
 rrdtool graph sound_counter_hour.png -v "sound_counter" --start -3600 --end now DEF:sound=sound_counter.rrd:sound:MAX LINE2:sound#00FF00
+rrdtool graph sound_counter.png -v "sound_counter" --start -86400 --end now DEF:sound=sound_counter.rrd:sound:MAX LINE2:sound#00FF00
+rrdtool graph sound_counter_week.png -v "sound_counter" --start -604800 --end now DEF:sound=sound_counter.rrd:sound:MAX LINE2:sound#00FF00
 rrdtool graph movement_counter_hour.png -v "movement_counter" --start -3600 --end now DEF:movement=movement_counter.rrd:movement:MAX LINE2:movement#00FF00
+rrdtool graph movement_counter.png -v "movement_counter" --start -86400 --end now DEF:movement=movement_counter.rrd:movement:MAX LINE2:movement#00FF00
+rrdtool graph movement_counter_week.png -v "movement_counter" --start -604800 --end now DEF:movement=movement_counter.rrd:movement:MAX LINE2:movement#00FF00
+rrdtool graph alarm_counter_hour.png -v "alarm_counter" --start -3600 --end now DEF:alarm=alarm_counter.rrd:alarm:MAX LINE2:alarm#00FF00
+rrdtool graph alarm_counter.png -v "alarm_counter" --start -86400 --end now DEF:alarm=alarm_counter.rrd:alarm:MAX LINE2:alarm#00FF00
+rrdtool graph alarm_counter_week.png -v "alarm_counter" --start -604800 --end now DEF:alarm=alarm_counter.rrd:alarm:MAX LINE2:alarm#00FF00
 
-cp temperature.png /var/www/html/
-cp door.png /var/www/html/
-cp movement.png /var/www/html/
-cp sound.png /var/www/html/
 
 cp temperature_hour.png /var/www/html/
+cp temperature.png /var/www/html/
+cp temperature_week.png /var/www/html/
 cp door_hour.png /var/www/html/
-cp movement_hour.png /var/www/html/
-cp sound_hour.png /var/www/html/
+cp door.png /var/www/html/
+cp door_week.png /var/www/html/
 cp sound_counter_hour.png /var/www/html/
+cp sound_counter.png /var/www/html/
+cp sound_counter_week.png /var/www/html/
 cp movement_counter_hour.png /var/www/html/
+cp movement_counter.png /var/www/html/
+cp movement_counter_week.png /var/www/html/
+cp alarm_counter_hour.png /var/www/html/
+cp alarm_counter.png /var/www/html/
+cp alarm_counter_week.png /var/www/html/
